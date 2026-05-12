10606b443495de868bd8c134eeb868f36920b7e7
OLIVIER-IRATABARA <oiratabara@gmail.com>
2026-05-12 14:11:04 +0200

    we fixed whole

--- frontend/React/src/App.jsx
+++ frontend/React/src/App.jsx
@@ -9,12 +9,10 @@ import Book from "./components/Book";
 import SignUp2 from "./components/SignUp2";
 import Public from "./components/Public";
 import EventDetail from "./components/detailEvent";
-<<<<<<< HEAD
 import BookingTicket from "./components/BookingTicket";
-=======
 import ContHost from "./components/ContHost";
-// import AboutUser from "./components/Aboutuser";
->>>>>>> ae098521196ad48a66a1e61cf3342265edf2a5be
+import AboutUser from "./components/Aboutuser";
+
 
 function App() {
   return (
@@ -24,17 +22,15 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home/>}/>
         <Route path="/host" element={<Host/>}/>
-        {/* <Route path="/about" element={<AboutUs/>}/> */}
+        <Route path="/about" element={<AboutUs/>}/>
         <Route path="/help" element={<Help/>}/>
         <Route path="/book" element={<Book/>}/>
       <Route path="/sign2" element={<SignUp2/>}/>
       <Route path="/" element={<Public/>}/>
       <Route path="/detailEvent/:id" element={<EventDetail/>}/>
-<<<<<<< HEAD
       <Route path="/BookingTicket" element={<BookingTicket/>}/>
-=======
       <Route path = "/conthost" element={<ContHost/>}/>
->>>>>>> ae098521196ad48a66a1e61cf3342265edf2a5be
+
       </Routes>
     // <Home/>
     // <AboutUser/>
